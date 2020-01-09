c2d609a26df0f3028298a6431606a86a983e54a6
Emil9 <emil.grabowski@interia.pl>
2020-01-09 20:27:05 +0100

    Post - rental

---
+++ routes/rentals.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Rental = require("../models/rental");
+
+router.get("/", (req,res,next)=>{
+    Rental.find().exec()
+    .then(docs => res.status(200).json(docs))
+    .catch(err =>res.status(500).json({error: err}));
+});
+
+router.post("/", (req,res,next)=>{
+    const rental = new Rental({
+        _id: new mongoose.Types.ObjectId(),
+        days: req.body.days
+    });
+    rental.save()
+    .then(result => {
+        res.status(201).json({
+            message: "Wypożyczono samochód",
+            createdRental: rental
+        });
+    })
+    .catch(err =>res.status(500).json({error:err}));
+});
