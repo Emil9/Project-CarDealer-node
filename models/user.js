7b7201506b6181cb167c4bf32e51a4e42e641606
Emil9 <emil.grabowski@interia.pl>
2020-01-09 22:21:29 +0100

    hash

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = mongoose.Schema({
         type: String, 
         required: true,
         unique: true,
+        math: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     },
     password: {type: String, required: true}
 });
