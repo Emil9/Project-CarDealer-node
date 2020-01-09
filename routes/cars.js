487e0cc53c63ca19806ee137f72c1174050c5e9a
Emil9 <emil.grabowski@interia.pl>
2020-01-09 19:43:27 +0100

    Post

--- routes/cars.js
+++ routes/cars.js
@@ -9,3 +9,22 @@ router.get("/", (req,res,next)=>{
     .then(docs => res.status(200).json(docs))
     .catch(err =>res.status(500).json({error: err}));
 });
+
+router.post("/", (req,res,next)=>{
+    console.log(req.file);
+    const car = new Car({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    car.save()
+    .then(result => {
+        res.status(201).json({
+            message: "Dodano nowy samochód",
+            createdCar: car
+        });
+    })
+    .catch(err =>res.status(500).json({error:err}));
+});
+
+module.exports = router;
