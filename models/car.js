a5d25c0ce6d52470d38abc97cad8cf63a0e368f9
Emil9 <emil.grabowski@interia.pl>
2020-01-09 21:59:46 +0100

    Add image in car

--- models/car.js
+++ models/car.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const carSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     name: String,
-    price: Number
+    price: Number,
+    productImage: String
 });
 
 module.exports = mongoose.model("Car", carSchema);
