1becae58774e9a7bd1f30c97ff92964031104f71
Emil9 <emil.grabowski@interia.pl>
2020-01-09 19:01:57 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb+srv://Projekt:<password>@projekt-wbv8q.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(morgan("dev"));
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 404);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
