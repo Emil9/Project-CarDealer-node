bd40dcec9bcd7dd1d0e2abd7ac31295f9c6606d1
Emil9 <emil.grabowski@interia.pl>
2020-01-09 22:28:59 +0100

    Add key to users

--- routes/users.js
+++ routes/users.js
@@ -46,4 +46,27 @@ router.delete("/:userId", (req, res, next) => {
     .catch(err => {res.status(500).json({error: err})})
 })
 
+router.post("/login", (req, res, next)=>{
+    User.findOne({email: req.body.email}).exec()
+    .then(user =>{
+        if(!user) {
+            return res.status(401).json({message: "Błąd autoryzacji"});
+        }
+        bcrypt.compare(req.body.password, user.password, (err, result) =>{
+            if(err){
+                return res.status(401).json({message: "Błąd autoryzacji"});
+            }
+            if(result){
+                const token = jwt.sign({
+                    email: user.email,
+                    userId: user._id
+                }, process.env.JWT_KEY, {expiresIn: "1h"})
+               return res.status(200).json({message: "Autoryzacja poprawna", token: token})
+            }
+            return res.status(401).json({message: "Błąd autoryzacji"});
+        });
+    })
+    .catch(err => {res.status(500).json({error: err})})
+});
+
 module.exports = router;
